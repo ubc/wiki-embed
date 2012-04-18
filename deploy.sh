93ee619ce60bd45e4c3cf0a419017f3b32799cf3
Eric Jackisch <eric@ejack.ca>
2012-04-18 16:06:51 -0700

    change svn username

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ GITPATH="$CURRENTDIR/" # this file should be in the base of your git repository
 # svn config
 SVNPATH="/tmp/$PLUGINSLUG" # path to a temp SVN repo. No trailing slash required and don't add trunk.
 SVNURL="http://plugins.svn.wordpress.org/wiki-embed" # Remote SVN repo on wordpress.org, with no trailing slash
-SVNUSER="enej" # your svn username
+SVNUSER="ctlt-dev" # your svn username
 
 
 # Let's begin...
