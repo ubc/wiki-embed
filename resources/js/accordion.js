6abaee5a2aa6bbeb785bb36f940a6748fbf251df
Enej Bajgoric <enej.bajgoric@gmail.com>
2013-11-19 11:00:24 -0800

    Autho Heaight for accordions

--- resources/js/accordion.js
+++ resources/js/accordion.js
@@ -1,13 +1,13 @@
 jQuery(document).ready( function($){ 
 	$(".wiki-embed-accordion").accordion({
-						"autoHeight":false,
-						"disabled":false,
-						"active":0,
-						"animated":"slide",
-						"clearStyle":false,
-						"collapsible":true,
-						"event":"click",
-						"fillSpace":false, 
-						"header":"h2"} ); 
-}); 
-				
+		"autoHeight":false,
+		"disabled":false,
+		"active":0,
+		"animated":"slide",
+		"clearStyle":false,
+		"collapsible":true,
+		"event":"click",
+		"fillSpace":false, 
+		'heightStyle': "content",
+		"header":"h2"} ); 
+});
